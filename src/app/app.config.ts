f69cd0d5afc16ef915027fb5f27ded1fe0213010
Leonardo Henrique <leonardo.gabriel.henrique.dev@gmail.com>
2025-06-11 00:00:25 -0300

    FEAT: Integrate PrimeNG components and update styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,39 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
+const CustomPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#e6f2ff',
+      100: '#b3daff',
+      200: '#80c2ff',
+      300: '#4daaff',
+      400: '#0267FF',
+      500: '#0255e6',
+      600: '#0243cc',
+      700: '#0131b3',
+      800: '#011f99',
+      900: '#010d80',
+      950: '#000a66',
+    }
+  },
+});
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: CustomPreset
+      }
+    })
+  ]
 };
--- src/app/interface-adapters/presenters/containers/login-page/login-page.component.ts
+++ src/app/interface-adapters/presenters/containers/login-page/login-page.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNgModule } from '../../../../frameworks/ui/primeng/primeng.module';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [CommonModule, PrimeNgModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
