a5e9e0a0e92b21d33b15474b60048ec6aecc3b42
Leonardo Henrique <leonardo.gabriel.henrique.dev@gmail.com>
2025-06-13 23:39:49 -0300

    REFACTOR: remove theme service and related components, update app configuration for NG-ZORRO integration
    - Removed ThemeStorageService and ThemeService along with their associated components and tests.
    - Updated app.config.ts to integrate NG-ZORRO i18n and removed PrimeNG configuration.
    - Deleted unused PrimeNgModule and theme toggle component files.
    - Adjusted login page styles and imports to reflect changes.
    - Added a new theme.less file for custom theming with NG-ZORRO.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,39 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import { definePreset } from '@primeng/themes';
-import Aura from '@primeng/themes/aura';
-
 import { routes } from './app.routes';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
-const CustomPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '#e6f2ff',
-      100: '#b3daff',
-      200: '#80c2ff',
-      300: '#4daaff',
-      400: '#0267FF',
-      500: '#0255e6',
-      600: '#0243cc',
-      700: '#0131b3',
-      800: '#011f99',
-      900: '#010d80',
-      950: '#000a66',
-    }
-  },
-});
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: CustomPreset
-      }
-    })
+    provideNzI18n(en_US),
+    importProvidersFrom(FormsModule),
+    provideHttpClient()
   ]
 };
--- src/app/interface-adapters/presenters/containers/login-page/login-page.component.ts
+++ src/app/interface-adapters/presenters/containers/login-page/login-page.component.ts
@@ -1,14 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { PrimeNgModule } from '../../../../frameworks/ui/primeng/primeng.module';
-//import { ThemeToggleComponent } from '../../components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-login-page',
   imports: [
     CommonModule,
-    PrimeNgModule,
-    //ThemeToggleComponent
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
