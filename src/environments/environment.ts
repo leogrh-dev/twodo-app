eaddf26232e4772acb5ed379a46c68ce8d90bc87
Leonardo Henrique <leonardo.gabriel.henrique.dev@gmail.com>
2025-07-31 21:52:00 -0300

    chore: changing environments googleClientId

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   backendUri: 'http://localhost:3000',
   graphqlUri: 'http://localhost:3000/graphql',
   uploadUri: 'http://localhost:3000/upload',
-  googleClientId: '195882463084-1a8crdgo2rof9bqtab1t1qmqf18p7a2h.apps.googleusercontent.com',
+  //googleClientId: 'Seu google client ID',
 };
